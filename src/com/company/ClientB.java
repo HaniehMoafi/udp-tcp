f1bfbd96b1bd777bdf23f3db4749c692f97eabca
haniehMoafi <hani.moafi74@gmial.com>
2020-08-29 12:37:51 +0430

    menu

--- src/com/company/ClientB.java
+++ src/com/company/ClientB.java
@@ -1,9 +1,54 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @Author: hanieh Moafi
  * @Date: 8/29/2020
  **/
 public class ClientB {
+
+    ServerSocket serverSocket;
+    ExecutorService pool;
+
+    public ClientB() throws IOException {
+        System.out.println("Enter to Constructor");
+        serverSocket = new ServerSocket(40000);
+        System.out.println("Server Socket is initialized");
+        pool = Executors.newFixedThreadPool(5);
+    }
+
+    public void run() throws IOException {
+        while (true) {
+            System.out.println("Server Socket starts listening");
+            Socket connectionSocket = serverSocket.accept();
+            System.out.println("Server Socket find a new connection");
+            //TCP (IP SRC, IP DST, PORT SRC, PORT DST)
+            ServerThread serverThread = new ServerThread(connectionSocket);
+            pool.execute(serverThread);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+
+            ClientB mainServer = new ClientB();
+            mainServer.run();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    public void serverRole(){
+
+    }
 
 }
---
+++ src/com/company/ServerThread.java
@@ -0,0 +1,68 @@
+package com.company;
+
+
+import java.io.*;
+import java.net.Socket;
+
+public class ServerThread implements Runnable {
+    Socket connectionSocket;
+    BufferedReader reader;
+    BufferedWriter writer;
+
+    public ServerThread(Socket connectionSocket) throws IOException {
+        this.connectionSocket = connectionSocket;
+        reader = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        writer = new BufferedWriter(new OutputStreamWriter(connectionSocket.getOutputStream()));
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Local IP : " +
+                connectionSocket.getLocalAddress().getHostAddress());
+        System.out.println("Local Port : " +
+                connectionSocket.getLocalPort());
+        System.out.println("Remote IP : " +
+                connectionSocket.getInetAddress().getHostAddress());
+        System.out.println("Remote Port : " +
+                connectionSocket.getPort());
+
+        while (true) {
+            try {
+                System.out.println("**chat start**");
+                String line = reader.readLine();
+                System.out.println("Server received " + line);
+                if (line.startsWith("hi ")) {
+                    String[] tokens = line.split(" ");
+                    String name = tokens[1];
+                    writer.write("Hello " + name + "\n");
+                    writer.flush();
+                }
+                else if (line.startsWith("bye")) {
+                    String[] tokens = line.split(" ");
+                    String word = tokens[1];
+                    writer.write("see you later " + word + "\n");
+
+                    writer.flush();
+                }
+                else if (line.startsWith("exit")) {
+                    break;
+                }
+                else {
+                    writer.write("Your keyword is not recognized\n");
+                    writer.flush();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+        try {
+            reader.close();
+            writer.close();
+            connectionSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
